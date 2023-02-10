202e593dde38f186a09e75ac353bb0b103007b27
Aldorithms <99375833+aldorithms@users.noreply.github.com>
2023-02-10 12:59:07 -0800

    Yes

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
-import fmt
 
-func main{
-	fmt.Println("Hello world!\n")
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+
+	firstName := "Aldo"
+	lastName := "Pittman"
+
+	var d uint8
+	d = 255
+
+	fmt.Printf("My name is %v %v \n", firstName, lastName)
+	fmt.Printf("My number is %v", d)
+	goodbye()
+}
+
+func goodbye() {
+	fmt.Println("Goodbye!")
 }
